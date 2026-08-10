f1049154f92c39288c6a4fca1bf9237da24f2a3a
Datta Punjare <dattapunjare3050@gmail.com>
2026-08-10 17:07:31 +0530

    Day_1 Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,8 +1,30 @@
-''' Name:  Datta Punjare
-    Batch: August
-    Day:   1
+''' Name:   Datta Punjare
+    Cohort: TEP cohort 2026
+    Day:    1
+    Date:   9 Aug
+
+    Description:
+    This file contains the assignment given on day 1.
+    It includes example of data type conversion, for loop, and while loop.
+
+    What I have done in day 1:
+
+    - Learned Python basics
+    - Learned why Python is used
+    - Learned about virtual environments
+    - Learned VS Code shortcuts
+    - Learned Google Colab
+    - Learned Kaggle
+    - Learned data types
+    - Learned dictionaries
+    - Learned slicing
+    - Learned if-else
+    - Learned loops
+
 '''
 
+
+ 
 #  Q1. use for loop 
 # Ask the user how many number they want add
 
@@ -26,3 +48,13 @@ while password != "admin":
     password = input("Enter password: ")
 
 print("Correct password")
+
+
+#Q3. Data type conversion (string to number)
+
+# String input
+num_str = "25"
+
+# Convert string to integer
+num_int = int(num_str)
+print("String to int:", num_int)
